78ab3828120dcc95858fe3b98dfa831583d88c1e
VICTOR MANUEL VILCHIS HERNANDEZ <superkallo3@gmail.com>
2024-12-09 20:59:07 -0600

    Delete gotoxy.cpp

--- gotoxy.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>  
-#include <windows.h>  
-void Gotoxy(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
